5b7b7098d3224019547e51919d6a7b656786de43
Githubmagali <magalivictoria85068@gmail.com>
2024-02-18 21:27:12 -0300

    fix cartprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { CartProvider } from "@/context/cartProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}</body>
+      <CartProvider>
+        {children}
+      </CartProvider>
+        </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,14 @@
 import UserPage from '@/components/user'
-import { CartProvider } from '../context/cartProvider'
-import MenuPage from './menu/menuPage'
+
 
 
  function HomePage(){
   return(
   <>
     <UserPage />
-  <CartProvider>
-  <MenuPage />
-  </CartProvider>
+    
+
+
     </>
 
   )
