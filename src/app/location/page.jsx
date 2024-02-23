653a450e82733890b6009ab062c8883fd2742c00
Githubmagali <magalivictoria85068@gmail.com>
2024-02-22 21:40:21 -0300

    location

--- src/app/location/page.jsx
+++ src/app/location/page.jsx
@@ -1,55 +1,68 @@
 function Location() {
     return (
         <>
+            <div className="flex items-center justify-center pt-24 pb-10">
+                <div className="grid grid-cols-2 text-center">
+                    <div className="flex items-center justify-center">
+                        <div className="w-96 h-96 overflow-scroll bg-white">
+                            <ul>
+                                <div className="grid grid-cols-2 py-4">
+                                    <div className="grid col-1">
+                                        <p className="text-gray-400 "><i class='bx bxs-phone-call'></i>+54 11 23 436</p>
+                                        <p className="text-gray-400 ">hamb90@mail.com</p>
+                                    </div>
+                                    <div className="grid col-2">
+                                        <p className=" text-lg">CABA</p>
+                                        <li className=" text-gray-400 ">Av. Galicia 100, B1868BGA Piñeyro</li>
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-2 py-4 ">
+                                    <div className="grid col-1">
+                                        <p className="text-gray-400 "><i class='bx bxs-phone-call'></i>+54 11 23 430</p>
+                                        <p className="text-gray-400 ">hamb92@mail.com</p>
+                                    </div>
+                                    <div className="grid col-2">
+                                        <p className=" text-lg">La Lucila</p>
+                                        <li className=" text-gray-400 ">San Lorenzo 300, B1637BMC La Lucila Buenos Aires</li>
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-2 py-4 ">
+                                    <div className="grid col-1">
+                                        <p className="text-gray-400 "><i class='bx bxs-phone-call'></i>+54 11 23 432</p>
+                                        <p className="text-gray-400 ">hamb93@mail.com</p>
+                                    </div>
+                                    <div className="grid col-2">
+                                        <p className=" text-lg">Benavidez</p>
+                                        <li className=" text-gray-400 ">Av. Luis Vernet 200, Benavidez</li>
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-2 py-4 ">
+                                    <div className="grid col-1">
+                                        <p className="text-gray-400 "><i class='bx bxs-phone-call'></i>+54 11 23 432</p>
+                                        <p className="text-gray-400 ">hamb93@mail.com</p>
+                                    </div>
+                                    <div className="grid col-2">
+                                        <p className=" text-lg">Benavidez</p>
+                                        <li className=" text-gray-400 ">Av. Luis Vernet 200, Benavidez</li>
+                                    </div>
+                                </div>
+                            </ul>
 
-            <div className="text-center pt-40 ">
-                <h1 className=" py-5 text-2xl">Our branches</h1>
-                <div className="grid grid-cols-2 text-center p-10">
-                    <div className="p-3 pl-80 pt-20">
-                        <h2 className="font-bold">Recoleta</h2>
-                        <p className=" ">Av Galicia 100, B1868BGA CABA</p>
+                        </div>
                     </div>
-                    <div className="h-56 w-96">
+                    <div className="">
                         <iframe
                             title="map"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.1372370683007!2d-58.38622642532899!3d-34.676485661342944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcccb8228f238d%3A0x4e7d2cae69803158!2sAv.%20Galicia%20100%2C%20B1868BGA%20Pi%C3%B1eyro%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1705963186092!5m2!1ses-419!2sar"
-                            className="w-full h-full"
-                            allowfullscreen="false"
+                            width="500"
+                            height="500"
+                            allowFullScreen={false}
                             loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"
+                            referrerPolicy="no-referrer-when-downgrade"
                         ></iframe>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 text-center ">
-                    <div className="p-3 pl-80 pt-20">
-                        <h2 className="font-bold ">Caballito</h2>
-                        <p className="">Av Galicia 100, B1868BGA CABA</p>
-                    </div>
-                    <div className="h-56 w-96">
-                        <iframe
-                            title="map"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.1372370683007!2d-58.38622642532899!3d-34.676485661342944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcccb8228f238d%3A0x4e7d2cae69803158!2sAv.%20Galicia%20100%2C%20B1868BGA%20Pi%C3%B1eyro%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1705963186092!5m2!1ses-419!2sar"
-                            className="w-full h-full"
-                            allowfullscreen="false"
-                            loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                    </div>
-                </div>
-                <div className="grid grid-cols-2 text-center p-10 ">
-                    <div className="p-3 pl-80 pt-20">
-                        <h2 className="font-bold">Villa Ourtuzar</h2>
-                        <p className="">Av Galicia 100, B1868BGA CABA</p>
-                    </div>
-                    <div className="h-56 w-96">
-                        <iframe
-                            title="map"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.1372370683007!2d-58.38622642532899!3d-34.676485661342944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcccb8228f238d%3A0x4e7d2cae69803158!2sAv.%20Galicia%20100%2C%20B1868BGA%20Pi%C3%B1eyro%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1705963186092!5m2!1ses-419!2sar"
-                            className="w-full h-full"
-                            allowfullscreen="false"
-                            loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"
-                        ></iframe>
+
+
+
                     </div>
                 </div>
             </div>
