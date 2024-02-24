f1752a8bbb149a665163c2384e71ab8b0de01494
Githubmagali <magalivictoria85068@gmail.com>
2024-02-24 18:48:47 -0300

    add purchasing

--- src/app/buyCart/purchasing/page.jsx
+++ src/app/buyCart/purchasing/page.jsx
@@ -1,13 +1,17 @@
+"use client"
+import React from "react";
+import { useCart } from "@/context/cartProvider";
 import FormBuy from '@/app/buyCart/formBuy/page'
 
 
 function Purchasing() {
+    const { cart, totalCost } = useCart();
 
     return (<>
         <div className="flex items-center justify-center h-screen bg-lime-50">
             <div className="grid grid-cols-2 gap-2 w-full">
                 <div className=" px-3">
-                    <div className="border-t-8 border-gray-500 px-48 rounded-xl"> </div>
+                    <div className="border-t-8 border-green-800 px-48 rounded-xl"> </div>
                     <h1 className="text-2xl pt-6 pb-7 pl-3 text-gray-500 ">Seleccione un método de envio</h1>
                     <div className="border border-gray-200 border-4 border-solid rounded-xl">
                         <div className="flex items-center justify-content left-0 pt-20 pb-3 lg:gap-x-16">
@@ -22,18 +26,28 @@ function Purchasing() {
                     </div>
                 </div>
                 <div className="px-3">
-                    <div className="border-t-8 border-gray-500 px-48 rounded-xl"> </div>
+                    <div className="border-t-8 border-green-800 px-48 rounded-xl"> </div>
                     <h1 className="text-2xl pt-6 pb-7 pl-3 text-gray-500 ">Resumen del pedido</h1>
-                    <div className="py-24 px-48 border border-gray-200 border-4 border-solid rounded-xl">
-                        <div className="flex justify-between">
-                          
-                        </div>
+                    <div className="relative py-24 px-48 border border-gray-200 border-4 border-solid rounded-xl overflow-scroll max-h-[2rem]">
+                    {cart.length > 0 ? (
+                    <ul className="absolute top-5 grid gap-y-4">
+                        {cart.map((item, index) => (
+                            <li key={index} className="text-center text-gray-500">
+                                {item.name.charAt(0).toUpperCase() + item.name.slice(1)} -{" "}
+                                {item.quantity} x ${item.price}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="text-center">No items in cart.</p>
+                )}
                     </div>
+                    <p className="text-center pt-3 text-gray-500 text-xl">Total Cost: ${totalCost}</p>
                 </div>
             </div>
         </div>
         <FormBuy />
-        </>
+    </>
     )
 }
 
